77f916a0472e0171f39a79c74db578e638370b69
loisemuthoni <loisemburu01@gmail.com>
2019-11-04 09:52:27 +0300

    portfolio hover effect

--- Js/app.js
+++ Js/app.js
@@ -27,4 +27,55 @@ $(document).ready(function() {
         $(".product").show();
         $(this).hide();
     })
+
+    //Portfolio hover effects
+    //##### Portfolio item 1 ####
+    $(".image").mouseenter(function() {
+        $(".para-1").show();
+    })
+    $(".image").mouseleave(function() {
+        $(".para-1").hide();
+    })
+    $(".image1").mouseenter(function() {
+        $(".para-2").show();
+    })
+    $(".image1").mouseleave(function() {
+        $(".para-2").hide();
+    })
+    $(".image2").mouseenter(function() {
+        $(".para-3").show();
+    })
+    $(".image2").mouseleave(function() {
+        $(".para-3").hide();
+    })
+    $(".image3").mouseenter(function() {
+        $(".para-4").show();
+    })
+    $(".image3").mouseleave(function() {
+        $(".para-4").hide();
+    })
+    $(".image4").mouseenter(function() {
+        $(".para-5").show();
+    })
+    $(".image4").mouseleave(function() {
+        $(".para-5").hide();
+    })
+    $(".image5").mouseenter(function() {
+        $(".para-6").show();
+    })
+    $(".image5").mouseleave(function() {
+        $(".para-6").hide();
+    })
+    $(".image6").mouseenter(function() {
+        $(".para-7").show();
+    })
+    $(".image6").mouseleave(function() {
+        $(".para-7").hide();
+    })
+    $(".image7").mouseenter(function() {
+        $(".para-8").show();
+    })
+    $(".image7").mouseleave(function() {
+        $(".para-8").hide();
+    })
 })
